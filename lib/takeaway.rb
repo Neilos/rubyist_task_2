85398d0eb45a129fd6c08dbeeb77f37c47474bf8
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-14 16:57:27 +0100

    Takeaway working as specified. All tests passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,38 +3,63 @@ require 'bundler/setup'
 require 'twilio-ruby'
 
 class Takeaway
-attr_reader :messenger
-attr_reader :menu
+  attr_reader :messenger
+  attr_reader :menu, :phone_number
 
-ACCOUNT_SID = 'AC72f58d844bc5b1a82a02fe1dad6bad71'
-AUTH_TOKEN = '120034c38f454a4f73029e420c43e8bd'
+  ACCOUNT_SID = 'AC72f58d844bc5b1a82a02fe1dad6bad71'
+  AUTH_TOKEN = '120034c38f454a4f73029e420c43e8bd'
 
+  def initialize(phone_number, menu={})
+    @menu = menu # an array of dish names and prices, e.g. {dish1 => 1.00, dish2 => 2.00}
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+    @messenger = @client.account.sms.messages
+    @phone_number = phone_number
+  end
 
-def initialize()
-  @menu = {}
-  @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-  @messenger = @client.account.sms.messages
-end
+  # menu_items should be a hash
+  # {dish1 => 1.00, dish2 => 2.00}
+  # where the dish names are the keys
+  # and the prices are the values
+  def add_to_menu(menu_items={})
+    menu.merge!(menu_items)
+  end
 
-def add_to_menu(menu_items={})
-  @menu.merge!(menu_items)
-end
+  # dish_names is an array of dish names 
+  # to be removed from the menu. 
+  def remove_from_menu(dish_names=[])
+    dish_names.each {|item| @menu.delete(item) }
+  end
 
-def remove_from_menu(menu_items=[])
-  menu_items.each {|item| @menu.delete(item) }
-end
+  # orders are given as a hash with two keys
+  # :expected_value => 5.00, 
+  # :dishes =>  ['pepperoni', 'cheese & tomato pizza']
+  # 
+  def process_order(order={})
+    expected_value  = order[:expected_value] || 0.00
+    dishes          = order[:dishes]         || []
+    customer_tel    = order[:telephone_number]
+    raise "Please confirm your order." unless expected_value == value_of(dishes)
+    send_text(customer_tel, phone_number, confirmation_message) if customer_tel
+  end
 
-def receive_order(order)
-  total_price = order[:total_price] || 0.00
-  dishes      = order[:dishes]      || []
-  total_price
-end
+private
 
-def send_text(to, from, message_body)
-  message = @messenger.create(
-    :body => message_body,
-    :to => to,
-    :from => from)
-end
+  def confirmation_message
+    (Time.now + 3600).strftime("Thank you. Your order has been placed and will be delivered before %H:%M")
+  end
+
+  # dishes should be an array of dish names
+  # which match dishes in the menu
+  def value_of(dishes=[])
+    dishes.inject(0){ |sum, dish| sum + menu[dish] }
+  end
+
+  # sends an SMS text message
+  def send_text(to, from, message_body)
+    message = @messenger.create(
+      :body => message_body,
+      :to => to,
+      :from => from)
+  end
 
 end
--- test/takeaway_test.rb
+++ test/takeaway_test.rb
@@ -7,50 +7,87 @@ require_relative '../lib/takeaway'
 describe Takeaway do
   
   before do
-    @takeaway = Takeaway.new
-    @to = "+447970261478"
+    @takeaway = Takeaway.new("+441290211259", 'meat feast pizza' => 2.00)
+    @customer_tel = "+447970261478"
     @from = "+441290211259"
   end
 
-  it "should send a text message" do
-    message = "Hi, Just testing with an SMS message"
-    @takeaway.messenger.expects(:create).with(to: @to, from: @from, body: message).returns(true).once
-    @takeaway.send_text(@to, @from, message)
+  it "should return a list of dishes with prices" do
+    expected_menu = {'meat feast pizza' => 2.00}
+    @takeaway.menu.must_equal expected_menu
   end
 
   it "should be possible to add dishes to the menu" do
-    @takeaway.add_to_menu('ham & pinapple pizza' => 1.00, 'cheese & tomato pizza' => 2.00)
-  end
-
-  it "should return a list of dishes with prices" do
-    @takeaway.add_to_menu('ham & pinapple pizza' => 1.00, 'cheese & tomato pizza' => 2.00)
-    expected_menu = {'ham & pinapple pizza' => 1.00, 'cheese & tomato pizza' => 2.00}
+    @takeaway.add_to_menu(
+      'ham & pinapple pizza' => 1.00,
+      'cheese & tomato pizza' => 2.00)
+    expected_menu = {
+      'meat feast pizza' => 2.00,
+      'ham & pinapple pizza' => 1.00,
+      'cheese & tomato pizza' => 2.00}
     @takeaway.menu.must_equal expected_menu
   end
 
   it "should be possible to remove dishes from the menu" do
-    @takeaway.add_to_menu('ham & pinapple pizza' => 1.00, 'cheese & tomato pizza' => 2.00)
-    @takeaway.remove_from_menu(['ham & pinapple pizza'])
-    expected_menu = {'cheese & tomato pizza' => 2.00}
+    @takeaway.remove_from_menu(["meat feast pizza"])
+    expected_menu = {}
     @takeaway.menu.must_equal expected_menu
   end
 
-  it "can receive an order" do
+  it "can process an order" do
+    @takeaway.stubs(:send_text).returns(true)
+    @takeaway.add_to_menu(
+      'ham & pinapple pizza' => 1.00,
+      'cheese & tomato pizza' => 2.00,
+      'pepperoni' => 3.00)
+    @takeaway.process_order(
+      :expected_value => 5.00, 
+      :telephone_number => @customer_tel,
+      :dishes =>  ['pepperoni', 'cheese & tomato pizza'])
+  end
+
+  it "can calculate the value of dishes" do
+    @takeaway.add_to_menu(
+      'ham & pinapple pizza' => 1.00,
+      'cheese & tomato pizza' => 2.00,
+      'pepperoni' => 3.00)
+    dishes = ['ham & pinapple pizza', 'cheese & tomato pizza', 'pepperoni']
+    @takeaway.send(:value_of, dishes)
+  end
+
+  it "should reject orders when the prices of dishes don't sum to equal the given total_prices" do
+    @takeaway.stubs(:send_text).returns(true)
+    @takeaway.add_to_menu(
+      'ham & pinapple pizza' => 1.00,
+      'cheese & tomato pizza' => 2.00,
+      'pepperoni' => 3.00)
+    lambda { @takeaway.process_order(
+              :expected_value => 5.00,
+              :dishes =>  ['pepperoni'], 
+              :telephone_number => @customer_tel) 
+    }.must_raise RuntimeError, "Please confirm your order."
+  end
+
+  it "should generate an appropriate confirmation message" do
+    expected_message = (Time.now + 3600).strftime("Thank you. Your order has been placed and will be delivered before %H:%M")
+    @takeaway.send(:confirmation_message).must_equal expected_message
+  end
+
+  it "should be able to send a text message" do
+    message = "Hi, Just testing with an SMS message"
+    @takeaway.messenger.expects(:create).with(to: @to, from: @from, body: message).returns(true).once
+    @takeaway.send(:send_text, @to, @from, message)
+  end
+
+  it "should send a text message confirmation if a valid order is processed" do
     @takeaway.add_to_menu('ham & pinapple pizza' => 1.00, 'cheese & tomato pizza' => 2.00, 'pepperoni' => 3.00)
-    @takeaway.receive_order(:total_price => 5.00, 
-                            :dishes =>  ['pepperoni',
-                                        'cheese & tomato pizza']).must_equal 5.00
+    expected_message = (Time.now + 3600).strftime("Thank you. Your order has been placed and will be delivered before %H:%M")
+    @takeaway.stubs(:send_text).returns(true)
+    @takeaway.process_order(
+      :expected_value => 5.00,
+      :dishes =>  ['pepperoni','cheese & tomato pizza'],
+      :telephone_number => @customer_tel)
   end
-
-  it "should.... " do
-    # @takeaway.messenger.stubs(:create).returns(true)
-
-  end
-
-
-
-
-
 
 
 end
